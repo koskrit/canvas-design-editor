11d56d6b0fbb7593d33e0a6f94de5e093bed9611
koskrit <koskritm@gmail.com>
2022-09-22 09:10:52 +0300

    🚀 Canvas Top Menu: Adds Set Background Color Functionality

--- web/src/components/pluginComponents/FabricJSCanvas/CanvasTopMenu/CanvasTopMenu.tsx
+++ web/src/components/pluginComponents/FabricJSCanvas/CanvasTopMenu/CanvasTopMenu.tsx
@@ -1,11 +1,15 @@
 import { Button, HStack } from '@chakra-ui/react'
 import { AiFillAccountBook } from 'react-icons/ai'
 
+import { setCanvasBackgroundColor } from 'src/plugins/fabricJSCanvas'
+
 const CanvasTopMenu = () => {
+  const itemRef = setCanvasBackgroundColor()
+
   return (
     <HStack w="full" p={2} background={'lightcoral'}>
       <Button>
-        <AiFillAccountBook />
+        <input ref={itemRef} type={'color'} />
       </Button>
       <Button>
         <AiFillAccountBook />
--- web/src/plugins/fabricJSCanvas.ts
+++ web/src/plugins/fabricJSCanvas.ts
@@ -19,9 +19,32 @@ export const addImageListener = (ImageUrl: string) => {
         console.log(oImg)
         fabricJSEditor?.canvas.add(oImg)
       })
+      console.log({ fabricJSEditor })
     }
 
     itemRef.current.onclick = () => addImageFunc(ImageUrl)
+  }, [fabricJSApi])
+
+  return itemRef
+}
+
+export const setCanvasBackgroundColor = () => {
+  const [fabricJSApi, setFabricJSAPi] = useGlobalState('fabricJSApi')
+  const itemRef = useRef()
+
+  useEffect(() => {
+    const { fabricJSEditor } = fabricJSApi
+
+    const setCanvasBackgroundColorFunc = (e) => {
+      console.log({ fabricJSEditor })
+
+      fabricJSEditor.canvas.setBackgroundColor(e.target.value, () => {
+        fabricJSEditor.zoomIn()
+        fabricJSEditor.zoomOut()
+      })
+    }
+
+    itemRef.current.onchange = (e) => setCanvasBackgroundColorFunc(e)
   }, [fabricJSApi])
 
   return itemRef
