f583f3a7b1898ff732fb88aa1b4d226b5b6f6eb9
koskrit <koskritm@gmail.com>
2022-09-23 04:13:26 +0300

    🚀 Canvas Side Menu: Adds Drawing Modes Functionality

--- web/src/components/pluginComponents/FabricJSCanvas/CanvasSideMenuShapes/CanvasSideMenuShapes.tsx
+++ web/src/components/pluginComponents/FabricJSCanvas/CanvasSideMenuShapes/CanvasSideMenuShapes.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 import {
   Button,
   Popover,
@@ -9,31 +11,67 @@ import {
 } from '@chakra-ui/react'
 import { AiFillCiCircle, AiFillIeSquare, AiOutlineLine } from 'react-icons/ai'
 
-import { addShape } from 'src/plugins/fabricJSCanvas'
+import { addShape, setCanvasAddShapeMode } from 'src/plugins/fabricJSCanvas'
+
+type Shape = 'Circle' | 'Rect' | 'Line'
 
 const CanvasSideMenuShapes = () => {
-  const circleRef = addShape('Circle')
-  const rectangleRef = addShape('Rectangle')
-  const lineRef = addShape('Line')
+  const circleRef = setCanvasAddShapeMode('Circle')
+  const rectangleRef = setCanvasAddShapeMode('Rect')
+  const lineRef = setCanvasAddShapeMode('Line')
+
+  const [parentShape, setParentShape] = useState<Shape>('Circle')
+
+  const getParentShapeIcon = () => {
+    switch (parentShape) {
+      case 'Circle':
+        return <AiFillCiCircle color="white" />
+      case 'Rect':
+        return <AiFillIeSquare color="white" />
+      case 'Line':
+        return <AiOutlineLine color="white" />
+    }
+  }
+  const setShapeHandler = (shape: Shape) => {
+    setParentShape(shape)
+  }
 
   return (
     <Popover>
       <PopoverTrigger>
         <Button background={'blue'} size="sm">
-          <AiFillCiCircle color="white" />
+          {getParentShapeIcon()}
         </Button>
       </PopoverTrigger>
       <PopoverContent w={'auto'}>
         <PopoverArrow />
         <PopoverCloseButton />
         <PopoverHeader p={0}>
-          <Button ref={circleRef} background={'blue'} size="sm" m={1}>
+          <Button
+            ref={circleRef}
+            onClick={() => setShapeHandler('Circle')}
+            background={'blue'}
+            size="sm"
+            m={1}
+          >
             <AiFillCiCircle color="white" />
           </Button>
-          <Button ref={rectangleRef} background={'blue'} size="sm" m={1}>
+          <Button
+            ref={rectangleRef}
+            onClick={() => setShapeHandler('Rect')}
+            background={'blue'}
+            size="sm"
+            m={1}
+          >
             <AiFillIeSquare color="white" />
           </Button>
-          <Button ref={lineRef} background={'blue'} size="sm" m={1}>
+          <Button
+            ref={lineRef}
+            onClick={() => setShapeHandler('Line')}
+            background={'blue'}
+            size="sm"
+            m={1}
+          >
             <AiOutlineLine color="white" />
           </Button>
         </PopoverHeader>
--- web/src/plugins/fabricJSCanvas.ts
+++ web/src/plugins/fabricJSCanvas.ts
@@ -39,8 +39,7 @@ export const setCanvasBackgroundColor = () => {
       console.log({ fabricJSEditor })
 
       fabricJSEditor.canvas.setBackgroundColor(e.target.value, () => {
-        fabricJSEditor.zoomIn()
-        fabricJSEditor.zoomOut()
+        fabricJSEditor.canvas.renderAll()
       })
     }
 
@@ -59,6 +58,7 @@ export const setCanvasModeDrawing = (enabled: boolean) => {
 
     const setCanvasModeDrawingFunc = (enabled: boolean) => {
       fabricJSEditor.canvas.isDrawingMode = enabled
+      fabricJSEditor.canvas.__eventListeners = {}
     }
 
     itemRef.current.onclick = (e) => setCanvasModeDrawingFunc(enabled)
@@ -67,7 +67,7 @@ export const setCanvasModeDrawing = (enabled: boolean) => {
   return itemRef
 }
 
-type Shape = 'Rectangle' | 'Circle' | 'Line'
+type Shape = 'Rect' | 'Circle' | 'Line'
 
 export const addShape = (shape: Shape) => {
   const [fabricJSApi, setFabricJSAPi] = useGlobalState('fabricJSApi')
@@ -95,6 +95,7 @@ export const setCanvasModeSelection = () => {
 
     const setCanvasModeSelectionFunc = () => {
       fabricJSEditor.canvas.isDrawingMode = false
+      fabricJSEditor.canvas.__eventListeners = {}
     }
 
     itemRef.current.onclick = (e) => setCanvasModeSelectionFunc()
@@ -111,11 +112,184 @@ export const deleteCanvasSelection = () => {
     const { fabricJSEditor } = fabricJSApi
 
     const deleteCanvasSelectionFunc = () => {
-      fabricJSEditor.canvas.remove(fabricJSEditor.canvas.getActiveObject())
+      const { canvas } = fabricJSEditor
+      const active = canvas.getActiveObject()
+      if (active) {
+        canvas.remove(active)
+        if (active.type == 'activeSelection') {
+          active.getObjects().forEach((obj) => canvas.remove(obj))
+          canvas.discardActiveObject().renderAll()
+        }
+      }
     }
 
     itemRef.current.onclick = (e) => deleteCanvasSelectionFunc()
   }, [fabricJSApi])
 
   return itemRef
+}
+
+/* ----------------------- Add Canvas Event Listeners ----------------------- */
+
+export const setCanvasAddShapeMode = (shape: Shape) => {
+  const [fabricJSApi, setFabricJSAPi] = useGlobalState('fabricJSApi')
+  const itemRef = useRef()
+
+  useEffect(() => {
+    const { fabricJSEditor } = fabricJSApi
+    let action
+
+    switch (shape) {
+      case 'Line':
+        action = (e) => setCanvasDrawLine(fabricJSEditor.canvas)
+        break
+      case 'Rect':
+        action = (e) => setCanvasDrawSquare(fabricJSEditor.canvas)
+        break
+      case 'Circle':
+        action = (e) => setCanvasDrawCircle(fabricJSEditor.canvas)
+        break
+    }
+
+    itemRef.current.onclick = action
+  }, [fabricJSApi])
+
+  return itemRef
+
+  /* -------------------------- Canvas Event Actions -------------------------- */
+
+  function setCanvasDrawLine(canvas: any) {
+    let isDown, createdShape
+
+    canvas.__eventListeners = {}
+    canvas.on('mouse:down', function (e) {
+      if (canvas.getActiveObject()) {
+        return false
+      }
+      isDown = true
+      const pointer = canvas.getPointer(e.e)
+      const points = [pointer.x, pointer.y, pointer.x, pointer.y]
+
+      createdShape = new fabric.Line(points, {
+        strokeWidth: 5,
+        fill: 'red',
+        stroke: 'red',
+        originX: 'center',
+        originY: 'center',
+      })
+      canvas.add(createdShape)
+    })
+
+    canvas.on('mouse:move', function (e) {
+      if (!isDown) return
+      const pointer = canvas.getPointer(e.e)
+
+      console.log({ createdShape })
+      createdShape.set({ x2: pointer.x, y2: pointer.y })
+      canvas.renderAll()
+    })
+
+    canvas.on('mouse:up', function (e) {
+      isDown = false
+    })
+  }
+}
+
+function setCanvasDrawSquare(canvas: any) {
+  canvas.__eventListeners = {}
+
+  let rectangle, isDown, origX, origY
+
+  canvas.on('mouse:down', function (e) {
+    if (canvas.getActiveObject()) {
+      return false
+    }
+    const pointer = canvas.getPointer(e.e)
+
+    isDown = true
+    origX = pointer.x
+    origY = pointer.y
+
+    rectangle = new fabric.Rect({
+      left: origX,
+      top: origY,
+      fill: '',
+      stroke: 'red',
+      strokeWidth: 3,
+    })
+    canvas.add(rectangle)
+  })
+
+  canvas.on('mouse:move', function (e) {
+    if (!isDown) return
+    const pointer = canvas.getPointer(e.e)
+    if (origX > pointer.x) {
+      rectangle.set({ left: Math.abs(pointer.x) })
+    }
+    if (origY > pointer.y) {
+      rectangle.set({ top: Math.abs(pointer.y) })
+    }
+
+    rectangle.set({ width: Math.abs(origX - pointer.x) })
+    rectangle.set({ height: Math.abs(origY - pointer.y) })
+    canvas.renderAll()
+  })
+
+  canvas.on('mouse:up', function (e) {
+    isDown = false
+  })
+}
+
+function setCanvasDrawCircle(canvas: any) {
+  canvas.__eventListeners = {}
+  let circle, isDown, origX, origY
+
+  canvas.on('mouse:down', function (e) {
+    if (canvas.getActiveObject()) {
+      return false
+    }
+    isDown = true
+    const pointer = canvas.getPointer(e.e)
+    origX = pointer.x
+    origY = pointer.y
+    circle = new fabric.Circle({
+      left: origX,
+      top: origY,
+      originX: 'left',
+      originY: 'top',
+      radius: pointer.x - origX,
+      angle: 0,
+      fill: '',
+      stroke: 'red',
+      strokeWidth: 3,
+    })
+    canvas.add(circle)
+  })
+
+  canvas.on('mouse:move', function (e) {
+    if (!isDown) return
+    const pointer = canvas.getPointer(e.e)
+    let radius = Math.abs(origY - pointer.y) / 2
+    if (radius > circle.strokeWidth) {
+      radius -= circle.strokeWidth / 2
+    }
+    circle.set({ radius: radius })
+
+    if (origX > pointer.x) {
+      circle.set({ originX: 'right' })
+    } else {
+      circle.set({ originX: 'left' })
+    }
+    if (origY > pointer.y) {
+      circle.set({ originY: 'bottom' })
+    } else {
+      circle.set({ originY: 'top' })
+    }
+
+    canvas.renderAll()
+  })
+
+  canvas.on('mouse:up', function (e) {
+    isDown = false
+  })
 }
